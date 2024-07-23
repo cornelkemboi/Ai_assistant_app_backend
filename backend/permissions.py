cf982a85b52b7844cc24cd931985009baf2975c0
cornelkemboi <cornelkim39@gmail.com>
2024-07-23 18:55:33 +0300

    Revert "Add back end code"
    This reverts commit 1c262ad549267005edbd58fe8c4f14b9152cac50.

--- backend/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsUserAuthenticated(BasePermission):
-    def has_permission(self, request, view):
-        user_id = view.kwargs.get('user_id')
-        if str(request.user.id) != user_id:
-            return False
-        return True
